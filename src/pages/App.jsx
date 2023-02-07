b80f911fcdaa63b925bf68d4ecf2e86b6151cbb6
samweol <sejuyes@gmail.com>
2023-02-07 21:16:46 +0900

    Feat: 회원가입 후 홈페이지로 이동

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,15 @@
 import { React, useEffect, useState } from "react";
-import AppRouter from "./AppRouter";
 import { authService } from "../routes/firebase";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Comments from "./comment/comments";
+import Home from "./Home";
+import Auth from "./login/auth";
+import MakeWishes from "./makeWishes";
 
 function App() {
   const [init, setInit] = useState(false); // 화면 초기화 여부
   const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 여부
-
+  const navigate = useNavigate();
   // 아래의 코드를 firestore에서 가져오는 방식으로 수정해야 합니다.
   // 로그인 정보 전역관리 완료하면 수정 예정
   useEffect(() => {
@@ -15,12 +19,25 @@ function App() {
       } else {
         setIsLoggedIn(false);
       }
-      setInit(true); // user 정보 확인되면 초기화 완료
     });
   }, []);
 
+  useEffect(() => {
+    setInit(true); // user 정보 확인되면 초기화 완료
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/auth");
+    }
+  }, [isLoggedIn]);
+
   return (
-    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}</>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/auth" element={<Auth />}></Route>
+      <Route path="/comments" element={<Comments />}></Route>
+      <Route path="/makewish" element={<MakeWishes />}></Route>
+    </Routes>
   );
 }
 
--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,23 +1,22 @@
 //절구통 코멘트 관련 모듈 import
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Comments from "./comment/comments";
 import Home from "./Home";
 import Auth from "./login/auth";
 import MakeWishes from "./makeWishes";
 
 const AppRouter = ({ isLoggedIn }) => {
-  console.log(isLoggedIn);
   return (
     <Router>
       <Routes>
-        {/*로그인 성공 시 Home으로, 실패 시 auth로 넘어갑니다.*/}
-        {isLoggedIn ? (
-          <Route path="/" element={<Home />}></Route>
-        ) : (
-          <Route path="/auth" element={<Auth />}></Route>
-        )}
-
-        {/*코멘트 작성 화면 라우팅*/}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
         <Route path="/comments" element={<Comments />}></Route>
         <Route path="/makewish" element={<MakeWishes />}></Route>
       </Routes>
