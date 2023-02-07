adfc867e3f790fbd35cb14ac677270c8e730046e
zpqmdh <ysys1130@gmail.com>
2023-02-07 16:08:57 +0900

    FEAT: 로그인 기능과 user 정보 firestore에 저장

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,27 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import AppRouter from "./AppRouter";
+import { authService } from "../routes/firebase";
 
 function App() {
-  //로그인 됐는지 안됐는지 확인하는 작업 필요, 무조건 했다고 하겠습니다
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [init, setInit] = useState(false); // 화면 초기화 여부
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 여부
 
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  // 아래의 코드를 firestore에서 가져오는 방식으로 수정해야 합니다.
+  // 로그인 정보 전역관리 완료하면 수정 예정
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true); // user 정보 확인되면 초기화 완료
+    });
+  }, []);
+
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}</>
+  );
 }
 
 export default App;
