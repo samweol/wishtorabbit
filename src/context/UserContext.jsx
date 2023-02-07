e169071f6496c6352c7af9738360bb946283b564
samweol <sejuyes@gmail.com>
2023-02-07 20:51:39 +0900

    Feat: context API 파일

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export function UserContextrovider({ children }) {
+  const [user, setUser] = useState({
+    uid: "",
+    email: "",
+    password: "",
+    name: "",
+    wid: "",
+  });
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
